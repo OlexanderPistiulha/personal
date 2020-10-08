a3b6816b59eed89f882aae1b7644504b9efb01c8
AlexanderPistiulha <olexanderfreelance@gmail.com>
2020-10-08 16:20:12 +0300

    added class object Select and updated effects screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,5 +13,21 @@ const select = new Select('#select', {
         {id: '7', value: 'item__7', link: '#'},
         {id: '8', value: 'item__8', link: '#'},
     ]
+});
 
+const selectTwo = new Select('#select-two', {
+    placeholder: 'Анимация фона',
+    
+    // текст с item.value вместо preceholder
+    // selectedId: '4',
+    data:[
+        {id: '1', value: 'item__1', link: '#'},
+        {id: '2', value: 'item__2', link: '#'},
+        {id: '3', value: 'item__3', link: '#'},
+        {id: '4', value: 'item__4', link: '#'},
+        {id: '5', value: 'item__5', link: '#'},
+        {id: '6', value: 'item__6', link: '#'},
+        {id: '7', value: 'item__7', link: '#'},
+        {id: '8', value: 'item__8', link: '#'},
+    ]
 });
